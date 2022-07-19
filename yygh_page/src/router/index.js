891fe31390588326421e2de3e99f60912bc12f4d
wyj41 <1319301332@qq.com>
2022-07-19 20:42:50 +0800

    数据字典

--- yygh_page/src/router/index.js
+++ yygh_page/src/router/index.js
@@ -65,7 +65,22 @@ export const constantRouterMap = [
       }
     ]
   },
-
+  {
+    path: '/cmn',
+    component: Layout,
+    redirect: '/cmn/list',
+    name: '数据管理',
+    alwaysShow: true,
+    meta: { title: '数据管理', icon: 'el-icon-s-help' },
+    children: [
+      {
+        path: 'list',
+        name: '数据字典',
+        component: () => import('@/views/dict/list'),
+        meta: { title: '数据字典', icon: 'table' }
+      }
+    ]
+  },
   {
     path: '/form',
     component: Layout,
